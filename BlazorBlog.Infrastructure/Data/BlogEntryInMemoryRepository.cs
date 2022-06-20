83736e6704d7e596d5b630f286170af7a8d23252
Leandro Gabriel Casas <leandrogabriel.casas@gmail.com>
2022-06-19 23:39:10 -0300

    Implementing inMemory repos

---
+++ BlazorBlog.Infrastructure/Data/BlogEntryInMemoryRepository.cs
@@ -0,0 +1,54 @@
+﻿using BlazorBlog.Core.Entities;
+using BlazorBlog.Core.Interfaces;
+
+namespace BlazorBlog.Infrastructure.Data;
+
+public class BlogEntryInMemoryRepository : IBlogEntryRepository
+{
+    private readonly InMemoryDataModel _model;
+
+    public BlogEntryInMemoryRepository()
+    {
+        _model = new InMemoryDataModel();
+    }
+
+    public Task<BlogEntry> CreateAsync(BlogEntry entity, CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task DeleteAsync(BlogEntry entity, CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IEnumerable<BlogEntry>> GetAllAsync(CancellationToken cancellationToken = default)
+    {
+        return Task.FromResult(_model.BlogEntries.AsEnumerable());
+    }
+
+    public Task<BlogEntry> GetByIdAsync<Tid>(Tid id, CancellationToken cancellationToken = default) where Tid : notnull
+    {
+        return GetByIdAsync<int>(id, cancellationToken);
+    }
+
+    private Task<BlogEntry> GetByIdAsync<T>(object id, CancellationToken cancellationToken)
+    {
+        return Task.FromResult(_model.BlogEntries.FirstOrDefault(b => b.Id == (int)id));
+    }
+
+    public Task<IEnumerable<BlogEntry>> GetByTitle(string title, CancellationToken cancellationToken = default)
+    {
+        return Task.FromResult(_model.BlogEntries.Where(b => String.Equals(b.Title, title, StringComparison.OrdinalIgnoreCase)));
+    }
+
+    public Task<IEnumerable<BlogEntry>> Search(string searchTerm, CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task UpdateAsync(BlogEntry entity, CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ BlazorBlog.Infrastructure/Data/InMemoryDataModel.cs
@@ -0,0 +1,34 @@
+﻿using BlazorBlog.Core.Entities;
+
+namespace BlazorBlog.Infrastructure.Data;
+
+internal class InMemoryDataModel
+{
+    internal IList<BlogEntry> BlogEntries { get; set; }
+    internal IList<Author> Authors { get; set; }
+
+    internal InMemoryDataModel()
+    {
+        var testAuthor = new Author()
+        {
+            Id = 0,
+            FullName = "Test Author",
+            EmailAddress = "test.author@blog.com"
+        };
+        Authors = new List<Author>() { testAuthor };
+        BlogEntries = new List<BlogEntry>()
+        {
+            new BlogEntry
+            {
+                Id = 0,
+                Title = "Test Blog Entry",
+                HtmlContent = "<h1>Test Blog Entry</h1><p>Lorem Ipson Dol Sit Amet</p>",
+                Url = "test_blog_entry",
+                Created = DateTime.Now,
+                Author = testAuthor,
+                LastEdited = DateTime.Now,
+                LastEditor = testAuthor
+            }
+        };
+    }
+}
